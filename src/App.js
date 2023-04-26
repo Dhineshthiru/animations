fe649af8b926a4845d6dcdb3912bf47b4d076b70
Dhinesh T <dhinesh@dorustree.in>
2023-04-26 13:06:10 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+import Filter from "./Filter";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=a5d698908ef1ea83324a1b4c9b78f1a4&language=en-US&page=1"
+    );
+    const movie = await data.json();
+    console.log(movie);
+    setPopular(movie.results);
+    setFiltered(movie.results);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Animation Filtering using 'Framer-motion'</h1>
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="popular-movies">
+      <AnimatePresence>
+        {filtered.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
